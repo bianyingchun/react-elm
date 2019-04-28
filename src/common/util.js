8a6ab2f462f1196a7f1b4bd57cc4c9f89c2c3222
bianyingchun <2093213209@qq.com>
2019-04-28 21:53:20 +0800

    x

--- src/common/util.js
+++ src/common/util.js
@@ -17,12 +17,15 @@ export function formatDistance(number) {
   if(number < 1000) return number+'米';
   return (number / 1000).toFixed(2) + '千米';
 }
-export function getImgaeUrl(path) {
+export function getImgaeUrl(path, is_bg) {
   const p1 = path.substring(0,1);
   const p2 = path.substring(1,3);
   const p3 = path.substring(3);
   let p4 = 'png';
   // const reg = /\.(jpg|jpeg|png|bmp)$/
   if(p3.lastIndexOf('jpeg')===p3.length-4) p4 = 'jpeg'
+  if(is_bg) {
+    return `https://fuss10.elemecdn.com/${p1}/${p2}/${p3}.${p4}?imageMogr/format/webp/thumbnail/750x/`
+  }
   return `https://fuss10.elemecdn.com/${p1}/${p2}/${p3}.${p4}?imageMogr/format/webp/thumbnail/!130x130r/gravity/Center/crop/130x130/`
  }
--- src/components/shop-detail/index.js
+++ src/components/shop-detail/index.js
@@ -7,42 +7,95 @@ import './style.scss'
 class ShopDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      show_activity_sheet:false
+    };
+    this.toggle_activity_sheet = this.toggle_activity_sheet.bind(this)
+  };
+  calc_shoptab_pos() {
+    console.log(this.refs);
+    // const pos = shoptab.get​Bounding​Client​Rect();
+    // console.log(pos)
   };
   componentDidMount() {
     const id = this.props.match.params.id;
     this.props.getShopDetail(id);
+    this.calc_shoptab_pos();
+  };
+  toggle_activity_sheet(isshow) {
+    this.setState({show_activity_sheet:isshow})
+  };
+  activity_sheet(activities) {
+    if(!this.state.show_activity_sheet) return null;
+
+    const items = activities.map((activity,index) => {
+      let styleObj = {
+        backgroundColor:'#'+activity.icon_color
+      }
+      return(
+      <div key={index} className="activity_item">
+        <span className="icon" style={styleObj}>{activity.icon_name}</span>
+        <span>{activity.tips}</span>
+      </div>
+      );
+    });
+    return (
+      <div className="activity_sheet">
+        <div className="mask" onClick={()=>{
+            this.toggle_activity_sheet(false)
+          }}></div>
+        <div className="content_wraper">
+          <span className="close_btn" onClick={()=>{
+            this.toggle_activity_sheet(false)
+          }}>关闭</span>
+          <div className="title">优惠活动</div>
+          <div className="alist">
+            {items}
+          </div>  
+        </div>
+      </div>
+      )
   };
   render() {
     const {rst, menu} = this.props;
-    if(!rst.name) return null
+    if(!rst.name) return null;
+    let navStyleObj = {
+      backgroundImage:`url(${getImgaeUrl(rst.shop_sign.image_hash,true)})`
+    }
     return (
-      <div>
+      <div className="shop_detail">
         <div className="info">
-          <div className="navbar">
+          <div className="navbar" style={navStyleObj}>
             <span>back</span>
           </div>
           <div className="content">
             <img className="avatar" alt="" src={getImgaeUrl(rst.image_path)}/>
-            <div className="base_info">
-              <div className="title">
-              {rst.name}
-              </div>
-              <div className="addition">
-                <span>评价{rst.rating}</span>
-                <span>月售{rst.recent_order_num}单</span>
-                <span>蜂鸟专送约{rst.order_lead_time}分钟</span>
-              </div>
-              <div className="activities">
-                <div className="short_cut">{ rst.activities[0].description}</div>
-                <span>{rst.activities && rst.activities.length}个优惠</span>
-              </div>
-              <div className="notice">
-              公告：欢迎光临，用餐高峰期请提前下单，谢谢。</div>
+            <div className="title">
+            {rst.name}
             </div>
+            <div className="addition">
+              <span>评价{rst.rating}</span>
+              <span>月售{rst.recent_order_num}单</span>
+              <span>蜂鸟专送约{rst.order_lead_time}分钟</span>
+            </div>
+            <div className="activities" onClick={()=>{
+            this.toggle_activity_sheet(true)
+          }}>
+              <span className="icon" style={{backgroundColor:'#'+rst.activities[0].icon_color}}>{rst.activities[0].icon_name}</span>
+              <div className="short_cut">{ rst.activities[0].description}</div>
+              <span className="count">{rst.activities && rst.activities.length}个优惠</span>
+            </div>
+            <div className="notice">
+            公告：欢迎光临，用餐高峰期请提前下单，谢谢。</div>
           </div>
+          {this.activity_sheet(rst.activities)}
         </div>
-        <div>
- 
+        <div className="shoptab" ref="shoptab">
+          <div className="tab_item active"><span>点餐</span></div>
+          <div className="tab_item"><span>评价</span></div>
+          <div className="tab_item"><span>商家</span></div>
+        </div>
+        <div className="tab_content">
         </div>
       </div>
     )
